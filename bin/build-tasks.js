2ec43b1d82f8b7386626bf55c2e64231805f9bb2
Andrzej Kopeć <kapkowski@gmail.com>
2018-03-05 21:27:41 +0100

    [Devmeeting RN] Build base structure for RN devmeeting

--- bin/build-tasks.js
+++ bin/build-tasks.js
@@ -1,18 +1,6 @@
 const path = require('path')
-const fs = require('fs')
 
-const pug = require('pug')
-
-const { checkProject, renderTemplate, projectExists, getProjects, renderProjectSelector } = require('./helpers')
-
-function getTemplates(base) {
-    return fs
-        .readdirSync(base)
-        .map(name => ({ name, base, path: path.join(base, name) }))
-        .filter(file => fs.statSync(file.path).isFile())
-        .filter(file => file.name.endsWith('.pug'))
-        .map(file => file.path)
-}
+const { renderTemplate, projectExists, getProjects, renderProjectSelector, getTemplates } = require('./helpers')
 
 (async () => {
     let project = process.argv[process.argv.length - 1]
